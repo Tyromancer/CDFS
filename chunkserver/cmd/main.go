79a1c3c9516a0ebb68ebe3249d528ded7023b225
Tyromancer <yangb7@hotmail.com>
2023-03-28 15:54:32 -0400

    Use command line arguments for chunkserver/cmd/main.go

--- chunkserver/cmd/main.go
+++ chunkserver/cmd/main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
-	"log"
-	"net"
-
+	"flag"
+	"fmt"
 	cs "github.com/tyromancer/cdfs/chunkserver"
 	pb "github.com/tyromancer/cdfs/pb"
 	"google.golang.org/grpc"
+	"log"
+	"net"
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":12345")
+	host := flag.String("host", "", "host address to listen on")
+	port := flag.Uint("port", 12345, "port number to listen on")
+	basePath := flag.String("path", "/CDFS", "base directory to store files")
+	masterHost := flag.String("mhost", "", "master server host address")
+	masterPort := flag.Uint("mport", 8080, "master server port number")
+	hbTimeout := flag.Int("hb", 100, "heartbeat timer timeout value (ms)")
+	flag.Parse()
+
+	// TODO: add command line argument checking
+
+	addr := fmt.Sprintf("%s:%d", *host, *port)
+	log.Println("Starting chunk server at ", addr)
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("Failed to listen on port 12345 %v", err)
 	}
 
 	s := cs.ChunkServer{
-		BasePath:   "/CDFS",
-		HostName:   "localhost",
-		Port:       12345,
-		MasterIP:   "localhost",
-		MasterPort: 8080,
+		BasePath:   *basePath,
+		HostName:   *host,
+		Port:       uint32(*port),
+		MasterIP:   *masterHost,
+		MasterPort: uint32(*masterPort),
 	}
 
 	grpcServer := grpc.NewServer()
@@ -33,7 +46,7 @@ func main() {
 	}
 
 	// Sending Heartbeat
-	timer := cs.HeartBeatTimer{Srv: &s, Timeout: 100}
+	timer := cs.HeartBeatTimer{Srv: &s, Timeout: *hbTimeout}
 	go timer.Trigger()
 
 	if err = grpcServer.Serve(lis); err != nil {
